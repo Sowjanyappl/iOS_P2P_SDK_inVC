6ce38cc239bb4f68563bd3b32938bb77080d2f1b
Sowjanyappl <sowjanya@peoplelinkvc.com>
2021-04-26 16:18:02 +0530

    Initial Commit

---
+++ InVC_iOS_SDK/InVC_iOS_SDK.h
@@ -0,0 +1,18 @@
+//
+//  InVC_iOS_SDK.h
+//  InVC_iOS_SDK
+//
+//  Created by Sowjanya Chavala on 26/04/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for InVC_iOS_SDK.
+FOUNDATION_EXPORT double InVC_iOS_SDKVersionNumber;
+
+//! Project version string for InVC_iOS_SDK.
+FOUNDATION_EXPORT const unsigned char InVC_iOS_SDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <InVC_iOS_SDK/PublicHeader.h>
+
+
